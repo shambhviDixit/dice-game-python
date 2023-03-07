fce33be8eaed4dd02397c1e72d420acf74c6136b
Shambhvi <99592580+shambhviDixit@users.noreply.github.com>
2023-03-07 21:41:27 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,47 @@
+import random
+response ="y"
+while(response=="y"):
+   no= random.randint(1,6)
+   if no == 1:
+      print("[-----]")
+      print("[     ]")
+      print("[  0  ]")
+      print("[     ]")
+      print("[-----]")
+   response==input("enter y to roll again and n to exit:")
+   if no == 2:
+         print("[-----]")
+         print("[     ]")
+         print("[-0-0-]")
+         print("[     ]")
+         print("[-----]")
+   response= input("enter y to roll again and n to exit:")
+   if no == 3:
+         print("[-----]")
+         print("[     ]")
+         print("[-000-]")
+         print("[     ]")
+         print("[-----]")
+   response= input("enter y to roll again and n to exit:")
+   if no == 4:
+         print("[-----]")
+         print("[0   0]")
+         print("[     ]")
+         print("[0   0]")
+         print("[-----]")
+   response= input("enter y to roll again and n to exit:")
+   if no == 5:
+         print("[-----]")
+         print("[0   0]")
+         print("[  0  ]")
+         print("[0   0]")
+         print("[-----]")
+   response= input("enter y to roll again and n to exit:")
+   if no == 6:
+         print("[-----]")
+         print("[0   0]")
+         print("[0   0]")
+         print("[0   0]")
+         print("[-----]")
+
+   response= input("enter y to roll again and n to exit:")
